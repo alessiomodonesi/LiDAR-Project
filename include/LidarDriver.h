14385d067c272be450e2f33d4a6ee2d0054dba80
Alessio Modonesi <alessiomodo2004@gmail.com>
2024-11-21 13:51:59 +0100

    init iniziale header

--- include/LidarDriver.h
+++ include/LidarDriver.h
@@ -1,11 +1,21 @@
 #include <iostream>
 #include <vector>
 
-class LidarDriver {
-        
-    public:
-        LidarDriver (void);
-        
-        double get_scan
-    private:
+class LidarDriver
+{
+
+public:
+    LidarDriver(void);
+    LidarDriver(int buffer_dim);
+
+    double new_scan(std::vector<double>);                 // memorizza nel buffer una scansione
+    std::vector<double> get_scan();                       // fornisce in output la scansione più vecchia e la rimuove dal buffer
+    void clear_buffer();                                  // elimina tutte le scansioni senza restituirle
+    std::vector<double> get_distance(double angle) const; // ritorna la lettura corrispondente a tale angolo
+
+private:
+    int buffer_dim{10};
+    int size_scansion{181};
 };
+
+std::ostream &operator<<(std::ostream &out, const std::vector<double> &last_scan);
