bd53e63a09dbaa01b022b647b480afaf06facb5c
Alessio Modonesi <alessiomodo2004@gmail.com>
2024-11-22 16:58:33 +0100

    Update LidarDriver.cpp

--- src/LidarDriver.cpp
+++ src/LidarDriver.cpp
@@ -4,8 +4,7 @@
 
 #include "LidarDriver.h"
 
-LidarDriver::LidarDriver(double resolution)
-    : angular_resolution_{resolution}, buffer(BUFFER_DIM, std::vector<double>((MAX_RANGE / angular_resolution_) + 1))
+LidarDriver::LidarDriver(double resolution) : angular_resolution_{resolution}
 {
     if (angular_resolution_ < 0.1 || angular_resolution_ > 1)
         throw std::invalid_argument("angular resolution must be [0.1, 1]");
