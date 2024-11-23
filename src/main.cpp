42e9ef8a7c70013639a09b6f6118630ee140d701
Alessio Modonesi <alessiomodo2004@gmail.com>
2024-11-23 12:46:49 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -29,20 +29,20 @@ int main()
     // simulazione di scansioni
     std::vector<double> scan1 = print_scan(0.0, 2.0, 181);
 
-    // // aggiunta delle scansioni al buffer
-    // default_lidar.new_scan(scan1);
+    // aggiunta delle scansioni al buffer
+    default_lidar.new_scan(scan1);
 
-    // // recupero e stampa della scansione più vecchia
-    // std::vector<double> oldest_scan = default_lidar.get_scan();
-    // std::cout << "scansione più vecchia:" << std::endl;
-    // std::cout << oldest_scan;
+    // recupero e stampa della scansione più vecchia
+    std::vector<double> oldest_scan = default_lidar.get_scan();
+    std::cout << "scansione più vecchia:" << std::endl;
+    std::cout << oldest_scan;
 
-    // // recupero della distanza a un angolo specifico
-    // double angle = 90.3;
-    // std::cout << "distanza a " << angle << "°: " << default_lidar.get_distance(angle) << " m" << std::endl;
+    // recupero della distanza a un angolo specifico
+    double angle = 90.3;
+    std::cout << "distanza a " << angle << "°: " << default_lidar.get_distance(angle) << " m" << std::endl;
 
-    // // pulizia del buffer
-    // default_lidar.clear_buffer();
+    // pulizia del buffer
+    default_lidar.clear_buffer();
     std::cout << "buffer pulito" << std::endl;
     return 0;
 }
