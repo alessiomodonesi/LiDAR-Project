f349d96c2f384d13da2e487e86e20ce5d19cc91e
Alessio Modonesi <alessiomodo2004@gmail.com>
2024-11-26 15:41:34 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -117,7 +117,8 @@ int main(int argc, char *argv[])
     std::vector<double> incomplete_scan = print_scan(0.0, 2.0, 134);
     std::cout << "vector incompleto\n" << incomplete_scan << std::endl;
     default_lidar.new_scan(incomplete_scan);
-    std::cout << "caso in cui scan contiene meno valori del dovuto\n" << default_lidar << std::endl;
+    incomplete_scan = default_lidar.get_scan();
+    std::cout << "caso in cui scan contiene meno valori del dovuto\n" << incomplete_scan << std::endl;
     
     // ------------------------ BLOCCO TEST 4 ------------------------
 
