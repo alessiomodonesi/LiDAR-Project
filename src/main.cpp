1ced99874a24369b616b164e7bc2f41ebcd340ad
Alessio Modonesi <alessiomodo2004@gmail.com>
2024-11-26 15:39:37 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -114,7 +114,6 @@ int main(int argc, char *argv[])
     // ------------------------ BLOCCO TEST 3 ------------------------
 
     // caso in cui scan contiene meno valori del dovuto
-    default_lidar.clear_buffer();
     std::vector<double> incomplete_scan = print_scan(0.0, 2.0, 134);
     std::cout << "vector incompleto\n" << incomplete_scan << std::endl;
     default_lidar.new_scan(incomplete_scan);
